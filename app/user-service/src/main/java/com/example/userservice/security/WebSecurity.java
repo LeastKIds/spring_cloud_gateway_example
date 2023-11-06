53175d18504e4302eb08b690f9d2e40a44e8d74b
leastkids <leastkids@gmail.com>
2023-11-06 07:44:31 +0000

    ADD - spring security1 of user-service

---
+++ app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,74 @@
+package com.example.userservice.security;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity{
+
+    
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(securityCorsConfigurationSource()))
+            .authorizeHttpRequests((auth) ->
+                auth
+                    .requestMatchers(
+                        AntPathRequestMatcher.antMatcher("/users/**"),
+                        AntPathRequestMatcher.antMatcher("/h2-console/**")
+                        )
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated() 
+            )
+            .headers((headers) ->
+                headers
+                    .frameOptions((frame) -> frame.disable())
+            )
+            .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurationSource() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedOrigins("/**")
+                    .allowCredentials(true);
+            }
+        };
+    }
+
+    @Bean
+    public CorsConfigurationSource securityCorsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("/**"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowCredentials(true);
+
+
+        config.addAllowedHeader("*");
+
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
