8c99ffc2f11327b9058ca0c9b27b6566351c876e
leastkids <leastkids@gmail.com>
2023-11-07 08:01:48 +0000

    jwt setting of user-service

--- app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ app/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,76 +1,71 @@
 package com.example.userservice.security;
 
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity{
 
+
+    public static final String ALLOWED_IP_ADDRESS = "172.17.0.1";
+    public static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS);
     
     @Bean
     public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
-            .cors(cors -> cors.configurationSource(securityCorsConfigurationSource()))
             .authorizeHttpRequests((auth) ->
                 auth
                     .requestMatchers(
-                        new AntPathRequestMatcher("/users/**"),
-                        new AntPathRequestMatcher("/h2-console/**"),
-                        new AntPathRequestMatcher("/heath_check/**"),
-                        new AntPathRequestMatcher("/user-service/**")
+                        // new AntPathRequestMatcher("/users/**"),
+                        // new AntPathRequestMatcher("/h2-console/**"),
+                        // new AntPathRequestMatcher("/heath_check/**"),
+                        // new AntPathRequestMatcher("/user-service/**")
+                        new AntPathRequestMatcher("/**")
                         )
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated() 
+                        .access(this::hasIpAddress)
+                    // .permitAll()
+                    // .anyRequest()
+                    // .authenticated() 
             )
             .headers((headers) ->
                 headers
                     .frameOptions((frame) -> frame.disable())
             )
+            .addFilter(getAuthenticationFilter())
             .build();
     }
+    
 
-    @Bean
-    public WebMvcConfigurer corsConfigurationSource() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                    .addMapping("/**")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedOrigins("/**")
-                    .allowCredentials(true);
-            }
-        };
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 
-    @Bean
-    public CorsConfigurationSource securityCorsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowCredentials(true);
-
-
-        config.addAllowedHeader("*");
-
-
-        source.registerCorsConfiguration("/**", config);
-        return source;
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
     }
 
 }
